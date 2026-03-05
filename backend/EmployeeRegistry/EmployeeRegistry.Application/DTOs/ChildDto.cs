c209b94a0eaa47af90933fecfb8ba8f507b160d3
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-03-06 05:03:53 +0600

    Create ChildDto.cs

---
+++ backend/EmployeeRegistry/EmployeeRegistry.Application/DTOs/ChildDto.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeRegistry.Application.DTOs
+{
+    public class ChildDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
