5034ba7710ecfaae9cb597aa3721c9582fd11539
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-03-06 05:03:09 +0600

    Create SpouseDto.cs

---
+++ backend/EmployeeRegistry/EmployeeRegistry.Application/DTOs/SpouseDto.cs
@@ -0,0 +1,9 @@
+namespace EmployeeRegistry.Application.DTOs
+{
+    public class SpouseDto
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string NID { get; set; } = string.Empty;
+    }
+}
