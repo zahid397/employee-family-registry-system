37d7d063ccb803a146d54cdb31eb63ff1d7e816f
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-03-06 07:49:08 +0600

    Create Program.cs

---
+++ backend/EmployeeRegistry/EmployeeRegistry.API/Program.cs
@@ -0,0 +1,46 @@
+using EmployeeRegistry.Infrastructure;
+using EmployeeRegistry.Infrastructure.Data;
+using EmployeeRegistry.Infrastructure.Seed;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+// Middleware
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Database migration + seed
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await db.Database.MigrateAsync();
+    await DatabaseSeeder.SeedAsync(db);
+}
+
+app.Run();
